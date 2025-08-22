104f46e9973e7e57ab919ae510068ad0635a53f4
hsjun <hippo8976@gmail.com>
2025-08-22 18:19:36 +0900

    feat: Project 생성 API구현 및 관련 Schema 수정

--- src/types/project-type.ts
+++ src/types/project-type.ts
@@ -1,10 +1,21 @@
 import { ProjectType } from '../generated/prisma';
+export interface CreateProjectDto {
+  title: string;
+  areaSize: number;
+  type: ProjectType;
+  description?: string;
+  durationWeeks?: number;
+  reviews?: string;
+  imageUrl?: string;
+  projectTags?: string[];
+  projectImages?: string[];
+}
 
 export type GetProjectsQuery = {
   page: number; // 1+
   pageSize: number; // 1~100
   q?: string; // 제목/설명 검색
-  type?: ProjectType;
+  type: ProjectType;
   tagIds?: number[]; // AND 필터 (모든 tag 포함)
   includeImages?: boolean; // 프로젝트-이미지 조인 포함
   includeTags?: boolean; // 프로젝트-태그 조인 포함
